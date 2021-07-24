019b9a6a16499ba55078185102c7ec9a49c64c39
Julian Thong <julianzthong@gmail.com>
2021-07-24 12:55:42 -0700

    alphabetBoardPath complete

--- algorithms/alphabetBoardPath.js
+++ algorithms/alphabetBoardPath.js
@@ -19,12 +19,68 @@
       }
   }
   let n = target.length;
-  let coords = [];
+  let coords = [[0,0]];
   for (let i = 0; i < n; i++) {
     coords.push(checkBoard(target[i]));
   }
 
+  const findPath = (coord1, coord2) => {
+    let diff = [(coord1[0] - coord2[0]), (coord1[1] - coord2[1])];
+    let y = diff[0];
+    let x = diff[1];
+    if (coord2[0] === 5) {
+      if (y === 0 && x === 0) { // LOTS OF EDGE CASES SMH
+        path += '!';
+        return;
+      }
+      if (y > 0) {
+        for (let i = 0; i < y; i++) {
+          path += 'U';
+        }
+      } else if (y < 0) {
+        for (let i = -1; i > y; i--) {
+          path += 'D';
+        }
+      }
+      if (x > 0) {
+        for (let i = 0; i < x; i++) {
+          path += 'L';
+        }
+      } else if (x < 0) {
+        for (let i = 0; i > x; i--) {
+          path += 'R';
+        }
+      }
+      path += 'D!';
+      return;
+    }
 
+    if (y > 0) {
+      for (let i = 0; i < y; i++) {
+        path += 'U';
+      }
+    } else if (y < 0) {
+      for (let i = 0; i > y; i--) {
+        path += 'D';
+      }
+    }
+    if (x > 0) {
+      for (let i = 0; i < x; i++) {
+        path += 'L';
+      }
+    } else if (x < 0) {
+      for (let i = 0; i > x; i--) {
+        path += 'R';
+      }
+    }
+    path += '!';
+  }
+
+  for (let i = 0; i < (coords.length - 1); i++) {
+    findPath(coords[i], coords[(i+1)]);
+  }
+
+  return path;
 };
 
-alphabetBoardPath('hello');
+
