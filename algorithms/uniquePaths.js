a6f480c329ba1cb1b5c21076a69f9af778235af4
Julian Thong <julianzthong@gmail.com>
2021-08-04 18:19:03 -0700

    uniquePaths completed.

---
+++ algorithms/uniquePaths.js
@@ -0,0 +1,58 @@
+/*
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+Example 1:
+Input: m = 3, n = 7
+Output: 28
+
+Example 2:
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+
+Example 3:
+Input: m = 7, n = 3
+Output: 28
+
+Example 4:
+Input: m = 3, n = 3
+Output: 6
+Constraints:
+
+1 <= m, n <= 100
+It's guaranteed that the answer will be less than or equal to 2 * 109.
+*/
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n, memo = {}) {
+  const key = m + ',' + n;
+  const reverseKey = n + ',' + m;
+  if (key in memo || reverseKey in memo) {
+    return memo[key]
+  }
+  if (m === 1 && n === 1) {
+    return 1;
+  }
+  if (n === 0 || m === 0) {
+    return 0;
+  }
+  memo[key] = uniquePaths(m-1, n, memo) + uniquePaths(m, n-1, memo);
+  memo[reverseKey] = memo[key];
+  return memo[key]
+};
+
+console.log(uniquePaths(3,2)) // 3
+console.log(uniquePaths(5,5)) // 70
+console.log(uniquePaths(4,4)) // 20
+console.log(uniquePaths(10,10)) // 48620
+console.log(uniquePaths(18,18)) // 2333606220
