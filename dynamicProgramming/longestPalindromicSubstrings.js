690801fcb81d59ebeed2e16adf268305ac7a9d10
Julian Thong <julianzthong@gmail.com>
2021-08-09 17:12:17 -0700

    Attempting longestPalindromicSubstrings

--- dynamicProgramming/longestPalindromicSubstrings.js
+++ dynamicProgramming/longestPalindromicSubstrings.js
@@ -26,5 +26,30 @@ s consist of only digits and English letters.*/
  * @return {string}
  */
  var longestPalindrome = function(s) {
+   let n = s.length;
+  const table = Array(n + 1)
+  .fill()
+  .map(()=>Array(n + 1).fill(false));
 
+  let lps = ''
+
+  // fill single letters
+  for (let i = 0; i < n; i++) {
+    table[i][i] = true;
+    lps = s.substring(i,i+1)
+  }
+
+  // double letters
+  for (let i = 0; i < n; i++) {
+    if (i + 1 < n && s[i] === s[i+1]) {
+      table[i][i+1] = true;
+      lps = s.substring(i, i+2);
+    }
+  }
+
+  for (let )
+  console.log(table)
+  console.log(lps);
 };
+
+longestPalindrome('babad');
