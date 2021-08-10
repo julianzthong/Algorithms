3eab9e3786fac011b381be5e24d6209007382475
Julian Thong <julianzthong@gmail.com>
2021-08-10 15:50:44 -0700

    Added base info

---
+++ algorithms/pascalsTriangle.js
@@ -0,0 +1,22 @@
+/*
+Given an integer numRows, return the first numRows of Pascal's triangle.
+
+Example 1:
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+Example 2:
+
+Example 2:
+Input: numRows = 1
+Output: [[1]]
+
+Constraints:
+1 <= numRows <= 30
+*/
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+ var generate = function(numRows) {
+
+};
