1db008a03f98820636ae5965c314c31f282bf648
julianzthong <julianzthong@gmail.com>
2021-08-25 15:51:56 -0700

    Attempting different approach for longest Palindrome

--- dynamicProgramming/longestPalindromicSubstrings.js
+++ dynamicProgramming/longestPalindromicSubstrings.js
@@ -25,37 +25,37 @@ s consist of only digits and English letters.*/
  * @param {string} s
  * @return {string}
  */
+// var longestPalindrome = function(s) {
+//   let n = s.length;
+//   const table = Array(n + 1)
+//   .fill()
+//   .map(()=>Array(n + 1).fill(false));
+
+//   let lps = '';
+
+//   // fill single letters
+//   for (let i = 0; i < n; i++) {
+//     table[i][i] = true;
+//     lps = s.substring(i,i+1)
+//   }
+
+//   // double letters
+//   for (let i = 0; i < n; i++) {
+//     if (i + 1 < n && s[i] === s[i+1]) {
+//       table[i][i+1] = true;
+//       lps = s.substring(i, i+2);
+//     }
+//   }
+//   // three+ letters
+//   for (let start = s.length - 1; start >= 0; start--) {
+//     for (let end = start + 2; end < s.length; end++) {
+//       table[start][end] = table[start+1][end-1] && s[start] === s[end];
+//       if (table[start][end]) {
+//         lps = lps.length < (end - start + 1) ? s.substring(start, end + 1) : lps;
+//       }
+//     }
+//   }
+//   return lps;
+// };
+
 var longestPalindrome = function(s) {
-   let n = s.length;
-  const table = Array(n + 1)
-  .fill()
-  .map(()=>Array(n + 1).fill(false));
-
-  let lps = '';
-
-  // fill single letters
-  for (let i = 0; i < n; i++) {
-    table[i][i] = true;
-    lps = s.substring(i,i+1)
-  }
-
-  // double letters
-  for (let i = 0; i < n; i++) {
-    if (i + 1 < n && s[i] === s[i+1]) {
-      table[i][i+1] = true;
-      lps = s.substring(i, i+2);
-    }
-  }
-  // three+ letters
-  for (let start = s.length - 1; start >= 0; start--) {
-    for (let end = start + 2; end < s.length; end++) {
-      table[start][end] = table[start+1][end-1] && s[start] === s[end];
-      if (table[start][end]) {
-        lps = lps.length < (end - start + 1) ? s.substring(start, end + 1) : lps;
-      }
-    }
-  }
-  return lps;
-};
-
-console.log(longestPalindrome('babad'));
