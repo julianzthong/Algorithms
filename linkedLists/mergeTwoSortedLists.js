87f8b6906b66c27fb14adfe99f8b9a9927f140ef
julianzthong <julianzthong@gmail.com>
2021-08-26 14:28:34 -0700

    finished mergeTwoSortedLists

--- linkedLists/mergeTwoSortedLists.js
+++ linkedLists/mergeTwoSortedLists.js
@@ -26,10 +26,55 @@ Both l1 and l2 are sorted in non-decreasing order.
  * }
  */
 /**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
+    if (!l1) return l2;
+    if (!l2) return l1;
 
+    let head = new ListNode(0);
+    let result = head;
+    while(l1 && l2) {
+
+        if (l1.val <= l2.val) {
+            head.next = new ListNode(l1.val);
+            head = head.next
+            l1 = l1.next;
+        } else if (l1.val >= l2.val) {
+            head.next = new ListNode(l2.val);
+            head = head.next
+            l2 = l2.next
+        } else {
+            head.next = new ListNode(l1.val);
+            head = head.next;
+            l1 = l1.next;
+
+            head.next = new ListNode(l2.val);
+            head = head.next;
+            l2 = l2.next;
+        }
+    }
+    while (l1 || l2) {
+        if (l1) {
+            head.next = new ListNode(l1.val);
+            head = head.next;
+            l1 = l1.next;
+        }
+        if (l2) {
+            head.next = new ListNode(l2.val);
+            head = head.next;
+            l2 = l2.next;
+        }
+    }
+
+    return result.next;
 };
