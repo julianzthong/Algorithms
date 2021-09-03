ab1f940ca173a9e3040cf4af74ec5d4cca12dfe8
julianzthong <julianzthong@gmail.com>
2021-09-03 12:12:52 -0700

    w

--- algorithms/spiralMatrix.js
+++ algorithms/spiralMatrix.js
@@ -1,62 +1,57 @@
 function spiralCopy(inputMatrix) {
-
   const row = inputMatrix.length;
   const col = inputMatrix[0].length;
 
-
   let rs = 0;
   let cs = 0;
-  let re = col-1;
-  let ce = row-1;
+  let re = col - 1;
+  let ce = row - 1;
 
   const total = row * col;
   let ans = [];
-  if(row === 1){
-     leftToRight (0, 0, col-1);
-    return ans
+  if (row === 1) {
+    leftToRight(0, 0, col - 1);
+    return ans;
   }
-  if(col === 1 ){
-    topToBottom (0, 0, row-1);
+  if (col === 1) {
+    topToBottom(0, 0, row - 1);
     return ans;
   }
   let i = 0;
 
-
-  function leftToRight (k, start, end) {
-    console.log(row, col, start, end)
-     for(let i=start; i<=end; i++){
-       ans.push(inputMatrix[k][i]);
-     }
+  function leftToRight(k, start, end) {
+    console.log(row, col, start, end);
+    for (let i = start; i <= end; i++) {
+      ans.push(inputMatrix[k][i]);
+    }
   }
-  function topToBottom (k, start, end) {
-     let c = col-1-k;
-     for(let i=start; i<=end; i++){
-       ans.push(inputMatrix[i][c]);
-     }
-  }
-
-  function rightToLeft (k, start, end) {
-      let r = row-1-k;
-     for(let i=start; i>=end; i--){
-       ans.push(inputMatrix[r][i]);
-     }
-  }
-  function bottomToTop (k, start, end) {
-
-     for(let i=start; i>end; i--){
-       ans.push(inputMatrix[i][k]);
-
-     }
+  function topToBottom(k, start, end) {
+    let c = col - 1 - k;
+    for (let i = start; i <= end; i++) {
+      ans.push(inputMatrix[i][c]);
+    }
   }
 
-  while(ans.length < total){
-    leftToRight(i,rs,re-1);
+  function rightToLeft(k, start, end) {
+    let r = row - 1 - k;
+    for (let i = start; i >= end; i--) {
+      ans.push(inputMatrix[r][i]);
+    }
+  }
+  function bottomToTop(k, start, end) {
+    for (let i = start; i > end; i--) {
+      ans.push(inputMatrix[i][k]);
+    }
+  }
+
+  while (ans.length < total) {
+    leftToRight(i, rs, re - 1);
     rs++;
-    topToBottom(i,cs,ce-1);
+    topToBottom(i, cs, ce - 1);
     ce--;
-    rightToLeft(i,re,rs);
+    rightToLeft(i, re, rs);
     re--;
-    bottomToTop(i,ce+1,cs);
+    bottomToTop(i, ce + 1, cs);
     cs++;
     i++;
   }
